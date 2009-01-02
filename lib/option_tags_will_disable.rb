0bd365f2f1e3b207b870c49ac677be1933316d43
Tekin Suleyman <tekin@tekin.co.uk>
2009-01-02 16:14:10 +0000

    Updated to allow a Proc to be used to identify disabled and selected options from a collection and increased test coverage

--- lib/option_tags_will_disable.rb
+++ lib/option_tags_will_disable.rb
@@ -12,27 +12,44 @@ module ActionView
         InstanceTag.new(object, method, self, options.delete(:object)).to_collection_select_tag(collection, value_method, text_method, options, html_options)
       end
       
-      def options_for_select(container, selected = nil, disabled = [])
+      def options_for_select(container, selected = nil, disabled = nil)
         container = container.to_a if Hash === container
 
         options_for_select = container.inject([]) do |options, element|
           text, value = option_text_and_value(element)
           selected_attribute = ' selected="selected"' if option_value_selected?(value, selected)
-          disabled_attribute = ' disabled="disabled"' if option_value_selected?(value, disabled)
+          disabled_attribute = ' disabled="disabled"' if option_value_selected?(value, disabled) && disabled != nil
           options << %(<option value="#{html_escape(value.to_s)}"#{selected_attribute}#{disabled_attribute}>#{html_escape(text.to_s)}</option>)
         end
 
         options_for_select.join("\n")
       end
-      
-      def options_from_collection_for_select(collection, value_method, text_method, selected = nil, disabled = [])
+
+      def options_from_collection_for_select(collection, value_method, text_method, selected = nil, disabled = nil)
         options = collection.map do |element|
           [element.send(text_method), element.send(value_method)]
         end
-        options_for_select(options, selected, disabled)
+
+        if selected.is_a?(Proc)
+          selected_values = collection.map do |element|
+            element.send(value_method) if selected.call(element) 
+          end.compact
+        else
+          selected_values = selected
+        end
+
+        if disabled.is_a?(Proc)
+          disabled_values = collection.map do |element|
+            element.send(value_method) if disabled.call(element)
+          end.compact
+        else
+          disabled_values = disabled
+        end
+        
+        options_for_select(options, selected_values, disabled_values)
       end
       
-      def option_groups_from_collection_for_select(collection, group_method, group_label_method, option_key_method, option_value_method, selected_key = nil, disabled = [])
+      def option_groups_from_collection_for_select(collection, group_method, group_label_method, option_key_method, option_value_method, selected_key = nil, disabled = nil)
         collection.inject("") do |options_for_select, group|
           group_label_string = eval("group.#{group_label_method}")
           options_for_select += "<optgroup label=\"#{html_escape(group_label_string)}\">"
@@ -49,7 +66,7 @@ module ActionView
         add_default_name_and_id(html_options)
         value = value(object)
         selected_value = options.has_key?(:selected) ? options[:selected] : value
-        disabled_value = options.has_key?(:disabled) ? options[:disabled] : []
+        disabled_value = options.has_key?(:disabled) ? options[:disabled] : nil
         content_tag("select", add_options(options_for_select(choices, selected_value, disabled_value), options, selected_value), html_options)
       end
 
@@ -57,7 +74,7 @@ module ActionView
         html_options = html_options.stringify_keys
         add_default_name_and_id(html_options)
         value = value(object)
-        disabled_value = options.has_key?(:disabled) ? options[:disabled] : []
+        disabled_value = options.has_key?(:disabled) ? options[:disabled] : nil
         content_tag(
           "select", add_options(options_from_collection_for_select(collection, value_method, text_method, value, disabled_value), options, value), html_options
         )
